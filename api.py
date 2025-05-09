75d651681fa7f405c21bfb57d626704a7c8cc2ad
Aditya Raj <akaushik2@gitam.in>
2025-05-09 11:04:46 +0530

    Connection made between ml backend n nextjs frontend

--- api.py
+++ api.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from predict import predict_fruit
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
@@ -9,14 +18,20 @@ def read_root():
 
 @app.post("/predict")
 async def predict_image(file: UploadFile = File(...)):
+    
     if not file.content_type.startswith("image/"):
         raise HTTPException(status_code=400, detail="Invalid file type. Please upload an image.")
 
+    
     contents = await file.read()
 
-    prediction, confidence = predict_fruit(contents)
+    
+    try:
+        prediction, confidence = predict_fruit(contents)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Prediction failed: {str(e)}")
 
     return {
         "prediction": str(prediction),
-        "confidence": round(float(confidence), 4)  
+        "confidence": round(float(confidence), 4)
     }
